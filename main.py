24698b5e7c76fd927b8667192fe337e424e0eedd
Yena Moon <119569513+yenamoon@users.noreply.github.com>
2023-06-10 13:58:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from function import *
+ 
+R = {(1, 1), (1, 3), (2, 2), (3, 3),(3, 1), (3, 4), (4, 4), (4, 3)}
+a = set(sum([[*i] for i in R],[]))
 
-check_reflexive(R)
+print(check_reflexive(R))
 
-check_symmetric(R)
+print(check_symmetric(R))
 
-check_transitive(R)
+print(check_transitive(R))
 
-check_equivalnce(R)
+print(check_equivalnce(R))
